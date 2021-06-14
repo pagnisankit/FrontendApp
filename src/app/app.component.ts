5dc3f7db6228c7dde1542e8158ca773416536c52
ankitPagnis <ankit_pagnis@yahoo.com>
2021-06-14 17:02:59 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'Multiple Choice Questions';
+  users : any;
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData(){
+    this.http.get("http://localhost:52428/api/Exam").subscribe(response=>{
+      this.users=response;
+    }, error => {
+      console.log(error);
+    })
+  }
+
 }
